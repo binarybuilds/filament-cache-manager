106b0b2a09be376ec570c4c6b7d081548aaa2e53
Srinath Reddy Dudi <srinathreddydudi@gmail.com>
2025-10-12 22:39:48 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# filament-cache-manager
+
+**Manage your application cache**
+
+[![Latest Version on Packagist](https://img.shields.io/packagist/v/binarybuilds/filament-cache-manager.svg?style=flat-square)](https://packagist.org/packages/binarybuilds/filament-cache-manager)
+[![GitHub Tests Action Status](https://img.shields.io/github/actions/workflow/status/binarybuilds/filament-cache-manager/run-tests.yml?branch=main&label=tests&style=flat-square)](https://github.com/binarybuilds/filament-cache-manager/actions?query=workflow%3Arun-tests+branch%3Amain)
+[![GitHub Code Style Action Status](https://img.shields.io/github/actions/workflow/status/binarybuilds/filament-cache-manager/fix-php-code-style-issues.yml?branch=main&label=code%20style&style=flat-square)](https://github.com/binarybuilds/filament-cache-manager/actions?query=workflow%3A"Fix+PHP+code+styling"+branch%3Amain)
+[![Total Downloads](https://img.shields.io/packagist/dt/binarybuilds/filament-cache-manager.svg?style=flat-square)](https://packagist.org/packages/binarybuilds/filament-cache-manager)
+
+
+
+
+## Testing
+
+```bash
+composer test
+```
+
+## Changelog
+
+Please see [CHANGELOG](CHANGELOG.md) for more information on what has changed recently.
+
+## Contributing
+
+Please see [CONTRIBUTING](.github/CONTRIBUTING.md) for details.
+
+## Security Vulnerabilities
+
+Please review [our security policy](../../security/policy) on how to report security vulnerabilities.
+
+## Credits
+
+- [Srinath Reddy Dudi](https://github.com/srinathreddydudi)
+- [All Contributors](../../contributors)
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
---
+++ tests/TestCase.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace BinaryBuilds\FilamentCacheManager\Tests;
+
+use BinaryBuilds\FilamentCacheManager\FilamentCacheManagerServiceProvider;
+use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
+use BladeUI\Icons\BladeIconsServiceProvider;
+use Filament\Actions\ActionsServiceProvider;
+use Filament\FilamentServiceProvider;
+use Filament\Forms\FormsServiceProvider;
+use Filament\Infolists\InfolistsServiceProvider;
+use Filament\Notifications\NotificationsServiceProvider;
+use Filament\Support\SupportServiceProvider;
+use Filament\Tables\TablesServiceProvider;
+use Filament\Widgets\WidgetsServiceProvider;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Livewire\LivewireServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
+
+class TestCase extends Orchestra
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Factory::guessFactoryNamesUsing(
+            fn (string $modelName) => 'BinaryBuilds\\FilamentCacheManager\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
+        );
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            ActionsServiceProvider::class,
+            BladeCaptureDirectiveServiceProvider::class,
+            BladeHeroiconsServiceProvider::class,
+            BladeIconsServiceProvider::class,
+            FilamentServiceProvider::class,
+            FormsServiceProvider::class,
+            InfolistsServiceProvider::class,
+            LivewireServiceProvider::class,
+            NotificationsServiceProvider::class,
+            SupportServiceProvider::class,
+            TablesServiceProvider::class,
+            WidgetsServiceProvider::class,
+            FilamentCacheManagerServiceProvider::class,
+        ];
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+        config()->set('database.default', 'testing');
+    }
+}
